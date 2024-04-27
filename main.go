c41b1c5dd78e661841fc35292003f9532d1dfeb3
hujaafar <hjawady703@gmail.com>
2024-04-27 13:51:03 +0300

    go

--- main.go
+++ main.go
@@ -9,166 +9,159 @@ import (
 
 func main() {
 	args := os.Args[1:]
-
-	if len(args) == 2 {
-		Text, _ := os.ReadFile(args[0])
-
-		array := strings.Split(string(Text), " ")
-		for i, chr := range array {
-			if chr == "(up)" {
-				if i == 0 {
-					fmt.Println("Error")
+	vwl := []string{"a", "o", "u", "e", "A", "O", "U", "E" , "I" , "i"}
+	if len(args) != 2 {
+		fmt.Println("Error")
+		os.Exit(1)
+	} else {
+		input, err := os.ReadFile(args[0])
+		if err != nil {
+			fmt.Println("Error")
+			os.Exit(1)
+		}
+		arr := strings.Split(string(input), " ")
+		for i, v := range arr {
+			if v == "(hex)" && i == 0 {
+				fmt.Println("Error")
+				os.Exit(1)
+			} else if v == "(hex)" && i > 0 {
+				arr[i-1] = Hex(arr[i-1])
+				arr = append(arr[:i], arr[i+1:]...)
+			} else if v == "(bin)" && i == 0 {
+				fmt.Println("Err")
+				os.Exit(1)
+			} else if v == "(bin)" && i > 0 {
+				arr[i-1] = Bin(arr[i-1])
+				arr = append(arr[:i], arr[i+1:]...)
+			} else if v == "(up)" && i == 0 {
+				fmt.Println("Err")
+				os.Exit(1)
+			} else if v == "(up)" && i > 0 {
+				arr[i-1] = strings.ToUpper(arr[i-1])
+				arr = append(arr[:i], arr[i+1:]...)
+			} else if v == "(low)" && i == 0 {
+				fmt.Println("Err")
+				os.Exit(1)
+			} else if v == "(low)" && i > 0 {
+				arr[i-1] = strings.ToLower(arr[i-1])
+				arr = append(arr[:i], arr[i+1:]...)
+			} else if v == "(cap)" && i == 0 {
+				fmt.Println("err")
+				os.Exit(1)
+			} else if v == "(cap)" && i > 0 {
+				arr[i-1] = Capital(arr[i-1])
+				arr = append(arr[:i], arr[i+1:]...)
+			} else if v == "(up," {
+				arr[i+1] = strings.TrimRight(arr[i+1], ")")
+				number, err := strconv.Atoi(arr[i+1])
+				if err != nil {
+					fmt.Println("Err")
 					os.Exit(1)
 				} else {
-					array[i-1] = strings.ToUpper(array[i-1])
-					array = append(array[:i], array[i+1:]...)
-				}
-			} else if chr == "(low)" {
-				if i == 0 {
-					fmt.Println("Error")
-					os.Exit(1)
-				} else {
-					array[i-1] = strings.ToLower(array[i-1])
-					array = append(array[:i], array[i+1:]...)
-				}
-			} else if chr == "(cap)" {
-				if i == 0 {
-					fmt.Println("Error")
-					os.Exit(1)
-				} else {
-					array[i-1] = capitalize(array[i-1])
-					array = append(array[:i], array[i+1:]...)
-					fmt.Println(array)
-				}
-			} else if chr == "(hex)" {
-				if i == 0 {
-					fmt.Println("Error")
-					os.Exit(1)
-				} else {
-					array[i-1] = hex(array[i-1])
-					array = append(array[:i], array[i+1:]...)
-				}
-			} else if chr == "(bin)" {
-				array[i-1] = BintoInt(array[i-1])
-				array = append(array[:i], array[i+1:]...)
-
-			} else if chr == "(up," {
-				if i == 0 {
-					fmt.Println("Error")
-					os.Exit(1)
-				} else {
-
-					b := strings.TrimRight(string(chr[i+1]), ")")
-					number, _ := strconv.Atoi(string(b))
-					for j := 1; j <= number; j++ {
-						array[i-j] = strings.ToUpper(array[i-j])
+					if number <= i {
+						for j := i - 1; number > 0; j-- {
+							number--
+							arr[j] = strings.ToUpper(arr[j])
+						}
+						arr = append(arr[:i], arr[i+2:]...)
 					}
-					array = append(array[:i], array[i+2:]...)
-
 				}
-			} else if chr == "(low," {
-				if i == 0 {
-					fmt.Println("Error")
+			} else if v == "(low," {
+				arr[i+1] = strings.TrimRight(arr[i+1], ")")
+				number, err := strconv.Atoi(arr[i+1])
+				if err != nil {
+					fmt.Println("Err")
 					os.Exit(1)
 				} else {
-					b := strings.TrimRight(string(array[i+1]), ")")
-					number, _ := strconv.Atoi(string(b))
-					for j := 1; j <= number; j++ {
-						array[i-j] = strings.ToLower(array[i-j])
+					if number <= i {
+						for j := i - 1; number > 0; j-- {
+							number--
+							arr[j] = strings.ToLower(arr[j])
+						}
+						arr = append(arr[:i], arr[i+2:]...)
 					}
-					array = append(array[:i], array[i+2:]...)
-
 				}
-			} else if chr == "(cap," {
-				b := strings.TrimRight(string(chr[i+1]), ")")
-				number, _ := strconv.Atoi(string(b))
-				for j := 1; j <= number; j++ {
-					array[i-j] = capitalize(array[i-j])
+			} else if v == "(cap," {
+				arr[i+1] = strings.TrimRight(arr[i+1], ")")
+				number, err := strconv.Atoi(arr[i+1])
+				if err != nil {
+					fmt.Println("Err")
+					os.Exit(1)
+				} else {
+					if number <= i {
+						for j := i - 1; number > 0; j-- {
+							number--
+							arr[j] = Capital(arr[j])
+						}
+						arr = append(arr[:i], arr[i+2:]...)
+					}
 				}
-				array = append(array[:i], array[i+2:]...)
+			} else if v == "a" || v == "A" {
+				w := arr[i+1]
+				if itHas(vwl, string(w[0])) {
+					if v == "a" {
+						arr[i] = "an"
+					} else {
+						arr[i] = "An"
+					}
+				}
 			}
 		}
-
-		punc(array)
+		punc(arr)
 		str := ""
-
-		for _, v := range array {
+		for _, v := range arr {
 			if v != "" && v != " " {
 				str += v + " "
 			}
 		}
 		str = strings.TrimRight(str, " ")
-		Write := os.WriteFile(args[1], []byte(str), 0644)
+		Write := os.WriteFile(args[1], []byte(str), 0o644)
 		if Write != nil {
-			fmt.Println("Error In Output")
+			fmt.Println("Err")
+			os.Exit(1)
 		} else {
-			fmt.Println("Done")
+			fmt.Println("Finish")
 			os.Exit(0)
 		}
-
-	} else {
-		fmt.Println("Missing file")
 	}
-}
-
-func hex(hex string) string {
-	number, _ := strconv.ParseInt(hex, 16, 64)
-	return fmt.Sprint(number)
-}
-
-func BintoInt(bin string) string {
-	number, _ := strconv.ParseInt(bin, 2, 64)
-	return fmt.Sprint(number)
-}
-func capitalize(chr string) string {
-	s := []rune(chr)
-	for i, v := range s {
-		if i == 0 && (v >= 'a' && v <= 'z') {
-			s[i] -= 32
-		} else if i > 0 && (v >= 'A' && v <= 'Z') {
-			s[i] += 32
-		}
-	}
-	return string(s)
 }
 
 func punc(s []string) []string {
-	punctuations := []string{".", ",", "!", "?", ":", ";"}
-	firstQ := true
+	punc := []string{".", ",", "!", "?", ":", ";"}
+	q := true
 	for i, v := range s {
-		isGroup := true
+		isGrouped := true
 		for _, l := range v {
-			if !Contain(punctuations, string(l)) {
-				isGroup = false
+			if !itHas(punc, string(l)) {
+				isGrouped = false
 				break
 			}
 		}
 		if v != "" {
-			if Contain(punctuations, v) && !isGroup {
+			if itHas(punc, v) && !isGrouped {
 				s[i-1] += v
 				s[i] = ""
-				//fmt.Print(s[i])
-			} else if Contain(punctuations, string(v[0])) && !isGroup {
+			} else if itHas(punc, string(v[0])) && !isGrouped {
 				s[i-1] += string(v[0])
 				s[i] = v[1:]
-			} else if !isGroup {
+			} else if !isGrouped {
 				for j, l := range v {
-					if Contain(punctuations, string(l)) && j != len(v)-1 {
+					if itHas(punc, string(l)) && j != len(v)-1 {
 						s[i] = v[:j+1] + " " + v[j+1:]
 					}
 				}
 			}
-			if isGroup {
+			if isGrouped {
 				s[i-1] += v
 				s[i] = ""
 			}
-			if firstQ && v == "'" {
+			if q && v == "'" {
 				if i+1 < len(s) {
 					s[i+1] = "'" + s[i+1]
 					s[i] = ""
-					firstQ = false
+					q = false
 				}
-			} else if !firstQ && v == "'" {
+			} else if !q && v == "'" {
 				if i > 0 {
 					if len(s[i-1]) == 0 {
 						s[i-2] = s[i-2] + "'"
@@ -176,7 +169,7 @@ func punc(s []string) []string {
 						s[i-1] = s[i-1] + "'"
 					}
 					s[i] = ""
-					firstQ = true
+					q = true
 				}
 			}
 		}
@@ -184,17 +177,35 @@ func punc(s []string) []string {
 	return s
 }
 
-func Contain(arr []string, a string) bool {
-	for _, v := range arr {
-		if v == a {
+func itHas(Array []string, s string) bool {
+	for _, ww := range Array {
+		if ww == s {
 			return true
 		}
 	}
 	return false
 }
 
-func Cap(hex string) string {
-	arr := []rune(hex)
+func Bin(z string) string {
+	dcml, err := strconv.ParseInt(z, 2, 64)
+	if err != nil {
+		fmt.Println("Err")
+		os.Exit(1)
+	}
+	return fmt.Sprint(dcml)
+}
+
+func Hex(z string) string {
+	dcml, err := strconv.ParseInt(z, 16, 64)
+	if err != nil {
+		fmt.Println("Err")
+		os.Exit(1)
+	}
+	return fmt.Sprint(dcml)
+}
+
+func Capital(z string) string {
+	arr := []rune(z)
 	for i, v := range arr {
 		if i == 0 && (v >= 97 && v <= 122) {
 			arr[i] -= 32
@@ -204,25 +215,4 @@ func Cap(hex string) string {
 		}
 	}
 	return string(arr)
-}
-
-func HexToDec(hex string) string {
-	decimal, err := strconv.ParseInt(hex, 16, 64)
-	if err != nil {
-		ErrorExit("Can't Convert Hex to Decimal")
-	}
-	return fmt.Sprint(decimal)
-
-}
-func BinToDec(hex string) string {
-	decimal, err := strconv.ParseInt(hex, 2, 64)
-	if err != nil {
-		ErrorExit("Can't Convert Bin to Decimal")
-	}
-	return fmt.Sprint(decimal)
-}
-
-func ErrorExit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
 }
